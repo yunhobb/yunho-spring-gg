68c4aa9d449d0268103af65d77058f51f0e43207
Jung Yunho <a01049048063@gmail.com>
2022-10-06 23:06:14 +0900

    feat: init setting

---
+++ src/main/java/com/example/yunhospringgg/YunhoSpringGgApplication.java
@@ -0,0 +1,13 @@
+package com.example.yunhospringgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunhoSpringGgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunhoSpringGgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yunhospringgg/YunhoSpringGgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yunhospringgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunhoSpringGgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
